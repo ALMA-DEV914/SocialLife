147bf325a993ffe6174f81c4dc37e99de385925e
ALMA_DEV914 <aungonalna58@gamil.com>
2022-03-10 10:03:56 -0800

    add routes for users and pots

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const helmet = require('helmet');
-const dotenv = require('dotenv');
-const res = require('express/lib/response');
-const userRoutes = require('./routes/users')
-const authRoute = require('./routes/auth')
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
+mongoose.connect(
+  process.env.MONGO_URL || 3000,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+//middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.use('/api/users', userRoutes);
-app.use('/api/auth', authRoute)
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
-app.get('/', (req, res) =>{
-    res.send("Welcome to my homepage")
-})
-app.get('/users', (req, res) =>{
-    res.send("Welcome to user homepage")
-})
-
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true
-}, ()=>{
-    console.log ('connected to mongodb')
-})
-
-app.listen(8000,()=>
-console.log('Backend server is running'))
+app.listen(3000, () => {
+  console.log("Backend server is running!");
+});
